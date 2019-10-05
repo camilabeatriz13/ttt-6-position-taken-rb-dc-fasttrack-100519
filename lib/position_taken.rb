e1d1f4a7759ff6b24cb3a03319cf9c4a118d8791
Camila Rodriguez <cr1015@georgetown.edu>
2019-10-05 19:13:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,13 @@
 # code your #position_taken? method here!
-def position_taken?
-  
+def position_taken?(board, index)
+  if board[index] == " " || board
+    return false
+  else
+    return true
+  end
+puts board
 end
+
+
+game_board = ["X", " ", "O", " ", " "," ", " ", " ", " "]
+position_taken?(game_board, 2)
